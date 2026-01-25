9b6231e3074f2f0f91f5a1a2044e110cdf0470fd
jallurividya <jallurividya2002@gmail.com>
2026-01-25 19:34:58 +0530

    evaluation

--- module4MockEvaluation2/routes/orders.routes.js
+++ module4MockEvaluation2/routes/orders.routes.js
@@ -1,14 +1,15 @@
 import express from "express"
 const orderRoute = express.Router()
 orderRoute.post("/add-order",(req,res)=>{
-
+    res.json({message: "Order added successfully"})
 })
 orderRoute.get("/get-my-orders/:customerId",(req,res)=>{
-
+    res.json({message: "Your order"})
 })
 orderRoute.put("/update-order/:orderId",(req,res)=>{
-
+res.json({message: "Order Updated"})
 })
 orderRoute.delete("/delete-order/:orderId",(req,res)=>{
-
+    res.json({message: "Order deleted"})
 })
+export default orderRoute
--- module4MockEvaluation2/server.js
+++ module4MockEvaluation2/server.js
@@ -1,9 +1,9 @@
 import express from "express"
-import orderroute from "./routes/orders.routes.js"
+import orderRoute from "./routes/orders.routes.js"
 const app = express()
 
 app.use(express.json)
-app.use("/",orderroute)
+app.use("/orders",orderRoute)
 
 app.listen(8080,()=>{
     console.log("server running")
