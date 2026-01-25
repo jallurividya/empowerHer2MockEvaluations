e5f050bac2b73b34191a42b273a97cdf9f011dd4
jallurividya <jallurividya2002@gmail.com>
2026-01-25 19:10:55 +0530

    routes added

---
+++ module4MockEvaluation2/routes/orders.routes.js
@@ -0,0 +1,14 @@
+import express from "express"
+const orderRoute = express.Router()
+orderRoute.post("/add-order",(req,res)=>{
+
+})
+orderRoute.get("/get-my-orders/:customerId",(req,res)=>{
+
+})
+orderRoute.put("/update-order/:orderId",(req,res)=>{
+
+})
+orderRoute.delete("/delete-order/:orderId",(req,res)=>{
+
+})
