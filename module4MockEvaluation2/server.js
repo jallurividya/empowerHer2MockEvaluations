e6458dd418881a295e0198183e9b2fcd3107c918
jallurividya <jallurividya2002@gmail.com>
2026-01-25 19:41:00 +0530

    evaluation

--- module4MockEvaluation2/server.js
+++ module4MockEvaluation2/server.js
@@ -2,8 +2,8 @@ import express from "express"
 import orderRoute from "./routes/orders.routes.js"
 const app = express()
 
-app.use(express.json)
-app.use("/orders",orderRoute)
+app.use(express.json())
+app.use("/",orderRoute)
 
 app.listen(8080,()=>{
     console.log("server running")
